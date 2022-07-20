8224c3b0ff2fd3f4a538f11112daaf1833c307d6
JordanJoseph <jordant.joseph@outlook.com>
2022-07-20 16:15:31 -0600

    fetch GET from trails database and list names of trails

--- src/components/TrailList.jsx
+++ src/components/TrailList.jsx
@@ -5,16 +5,18 @@ const TrailList = () => {
     const [trails, setTrails] = useState([]);
 
     useEffect(() => {
-        fetch("http://localhost:9292")
+        fetch("http://localhost:9292/trails")
         .then((r) => r.json())
         .then((trails) => setTrails(trails));
-    })
+    }, [])
 
     if (!trails) return <h2>Loading...</h2>
 
     return(
         <div> 
-            <h2>{trails}</h2>
+            {trails.map((trail) => 
+                <h1>{trail.name}</h1>
+            )}
         </div>
     )
 }
