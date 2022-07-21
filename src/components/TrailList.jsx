7ed8ea13bc98d5ceb41ee9eaba19b7acc70c2139
JordanJoseph <jordant.joseph@outlook.com>
2022-07-21 14:19:26 -0600

    install dependency for MUI data grid and add to TrailList component

--- src/components/TrailList.jsx
+++ src/components/TrailList.jsx
@@ -1,4 +1,7 @@
 import React, {useEffect, useState} from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+
 
 const TrailList = () => {
 
@@ -10,15 +13,90 @@ const TrailList = () => {
         .then((trails) => setTrails(trails));
     }, [])
 
+    /*
+    "id": 55,
+    "name": "Baltistan Peak",
+    "location": "Siachen Karakoram",
+    "distance": 336,
+    "elevation_gain": 6425,
+    "created_at": "2022-07-21T16:45:15.397Z",
+    "updated_at": "2022-07-21T16:45:15.397Z"
+    */
+
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 90 },
+        {
+          field: 'name',
+          headerName: 'Route',
+          width: 150,
+          editable: true,
+        },
+        {
+          field: 'location',
+          headerName: 'Location',
+          width: 150,
+          editable: true,
+        },
+        {
+          field: 'distance',
+          headerName: 'Distance',
+          type: 'number',
+          width: 110,
+          editable: true,
+        },
+        {
+          field: 'elevation_gain',
+          headerName: 'Elevation Gain',
+          sortable: false,
+          width: 160,
+        },
+    ];
+
+
     if (!trails) return <h2>Loading...</h2>
 
     return(
-        <div> 
+        <Box sx={{ height: 400, width: '100%' }}>
+            <DataGrid
+                rows={trails}
+                columns={columns}
+                pageSize={5}
+                rowsPerPageOptions={[5]}
+                checkboxSelection
+                disableSelectionOnClick
+            />
+      </Box>
+        /*<div> 
             {trails.map((trail) => 
                 <h1>{trail.name}</h1>
             )}
-        </div>
+        </div>*/
     )
 }
+
+/*
+import * as React from 'react';
+
+
+
+
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
+
+export default function DataGridDemo() {
+  return (
+
+  );
+}
+*/
 
 export default TrailList;
