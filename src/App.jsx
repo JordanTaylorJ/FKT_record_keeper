657b94a238207cbea641e49e1e7eb5ca4ef99750
JordanJoseph <jordant.joseph@outlook.com>
2022-08-04 14:47:30 -0600

    add theme color from MUI and clean out the default CSS

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,30 @@ import Home from './components/Home';
 import TrailList from './components/TrailList';
 import AthleteList from './components/AthleteList';
 import NavBar from './components/NavBar';
+import {ThemeProvider, createTheme } from '@mui/material/styles';
+
 
 
 function App() {
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#67a392',
+      },
+      secondary: {
+        main: '#9c9a56',
+        dark: '#757446', 
+      },
+      contrastThreshold: 3,
+      tonalOffset: 0.2,
+    },
+    components: {
+      MuiButton: {
+            color: ""
+      }
+    }
+  });
 
   const [trails, setTrails] = useState([
     //{athletes: []}
@@ -19,14 +40,16 @@ function App() {
   }, []);
 
   return (
-    <Router className='App'>
-      <NavBar/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="trails" element={<TrailList trails={trails}/>} />
-        <Route path="trails/athletes" element={<AthleteList trails={trails} setTrails={setTrails}/>}/>
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router className='App'>
+        <NavBar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="trails" element={<TrailList trails={trails}/>} />
+          <Route path="trails/athletes" element={<AthleteList trails={trails} setTrails={setTrails}/>}/>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
