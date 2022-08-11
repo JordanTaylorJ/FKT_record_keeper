997c688ed77a4fb07b3a26bb49b4fde3cd8df23f
JordanJoseph <jordant.joseph@outlook.com>
2022-08-10 18:13:13 -0600

    clean up code/formatting, work on form submit and updating state after submit

--- src/components/AthleteList.jsx
+++ src/components/AthleteList.jsx
@@ -15,154 +15,160 @@ import Paper from '@mui/material/Paper';
 const AthleteList = ({trails, setTrails}) => {
 
 
-    let location = useLocation();
+  let location = useLocation();
 
-    const thisTrail = trails.find(trail => trail.id == location.state.id)
-    const [athletes, setAthletes] = useState(thisTrail.athletes);
-    const [editAthleteId, setEditAthelteId] = useState(null);
-    const [editAthleteData, setEditAthleteData] = useState({
+  const thisTrail = trails.find(trail => trail.id == location.state.id)
+  const [athletes, setAthletes] = useState(thisTrail.athletes);
+  const [editAthleteId, setEditAthelteId] = useState(null);
+  const [editAthleteData, setEditAthleteData] = useState({
       name: "",
       time: "",
       trail_id: thisTrail,
       unsupported: false
   })
     
+    
+  const handleDeleteAthlete = (e) => {
+    e.preventDefault();
+    fetch(`http://localhost:9292/athletes/${e.target.value}`, {
+      method: 'DELETE',
+    })
+    .then((r) => r.json())
+    .then((deletedAthlete) => handleDeleteAthleteTrails(deletedAthlete))
+  }
 
-    const handleDeleteAthlete = (e) => {
-      fetch(`http://localhost:9292/athletes/${e.target.value}`, {
-        method: 'DELETE',
-      })
-      .then((r) => r.json())
-      .then((deletedAthlete) => handleDeleteAthleteTrails(deletedAthlete))
+  const handleDeleteAthleteTrails = (deletedAthlete) => {
+    const updateAthletes = athletes.filter((athlete) => athlete.id !== deletedAthlete.id);
+    const newTrails = trails.map((trail) => { 
+      if (trail.id === deletedAthlete.trail_id)
+        return ({
+          ...trail, 
+          athletes: [updateAthletes]
+        }); else {
+          return trail;
+        }
+    })
+    setAthletes(updateAthletes);
+    setTrails(newTrails);
+  }
+
+  const handleAddAthlete = (newAthlete) => {
+      fetch("http://localhost:9292/athletes", {
+          method: 'POST',
+          headers: { 
+            "Content-Type": "application/json", 
+          },
+          body: JSON.stringify(newAthlete),
+        })
+        .then(r => r.json())
+        .then((athlete) => handleAddAthleteToTrails(athlete))
+  }
+
+
+  const handleAddAthleteToTrails = (athlete) => {
+    const updateAthletes = [...athletes, athlete];
+    const newTrails = trails.map((trail) => { 
+      if (trail.id === athlete.trail_id)
+        return ({
+          ...trail, 
+          athletes: [updateAthletes]
+        }); else {
+          return trail;
+        }
+    })
+    setAthletes(updateAthletes);
+    setTrails(newTrails);
+  }  
+
+    
+  const handleEditAthleteSubmit = (e, updateAthlete) => {
+    e.preventDefault();
+    fetch(`http://localhost:9292/athletes/${e.target.value}`, {
+      method: 'PATCH',
+      headers: { 
+        "Content-Type": "application/json", 
+      },
+      body: JSON.stringify(updateAthlete),
+    })
+    .then((r) => r.json())
+    .then((updateAthlete) => handleEditAthleteTrails(updateAthlete))
+  }
+
+  const handleEditAthleteTrails = () => {
+
+  }
+  
+
+  const handleEditAthleteClick = (e, athlete) => {
+    e.preventDefault();
+    setEditAthelteId(athlete.id);
+
+    const formValues = {
+      name: athlete.name,
+      time: athlete.time,
+      trail_id: athlete.trailId,
+      unsupported: athlete.unsupported
     }
+    setEditAthleteData(formValues);
 
-    const handleDeleteAthleteTrails = (deletedAthlete) => {
-      const updateAthletes = athletes.filter((athlete) => athlete.id !== deletedAthlete.id);
-      const newTrails = trails.map((trail) => { 
-        if (trail.id === deletedAthlete.trail_id)
-          return ({
-            ...trail, 
-            athletes: [updateAthletes]
-          }); else {
-            return trail;
-          }
-      })
-      setAthletes(updateAthletes);
-      setTrails(newTrails);
-    }
+  }
 
-    const handleAddAthlete = (newAthlete) => {
-        fetch("http://localhost:9292/athletes", {
-            method: 'POST',
-            headers: { 
-              "Content-Type": "application/json", 
-            },
-            body: JSON.stringify(newAthlete),
-          })
-          .then(r => r.json())
-          .then((athlete) => handleAddAthleteToTrails(athlete))
-    }
+  const handleEditFormChange = (e) => {
+    e.preventDefault();
+    const athleteName = e.target.getAttribute("name")
+    const athleteValue = e.target.value 
 
+    const newAthleteData = {...editAthleteData }
+    newAthleteData[athleteName] = athleteValue 
 
-    const handleAddAthleteToTrails = (athlete) => {
-      const updateAthletes = [...athletes, athlete];
-      const newTrails = trails.map((trail) => { 
-        if (trail.id === athlete.trail_id)
-          return ({
-            ...trail, 
-            athletes: [updateAthletes]
-          }); else {
-            return trail;
-          }
-      })
-      setAthletes(updateAthletes);
-      setTrails(newTrails);
-    }  
-
-    /*
-    const handleEditAthlete = (e, updateAthlete) => {
-      fetch(`http://localhost:9292/athletes/${e.target.value}`, {
-        method: 'PATCH',
-        headers: { 
-          "Content-Type": "application/json", 
-        },
-        body: JSON.stringify(updateAthlete),
-      })
-      .then((r) => r.json())
-      .then((updateAthlete) => handleDeleteAthleteTrails(updateAthlete))
-    }
-    */
-
-    const handleEditAthleteClick = (e, athlete) => {
-      e.preventDefault();
-      setEditAthelteId(athlete.id);
-
-      const formValues = {
-        name: athlete.name,
-        time: athlete.time,
-        trail_id: athlete.trailId,
-        unsupported: athlete.unsupported
-      }
-      setEditAthleteData(formValues);
-
-    }
-
-    const handleEditFormChange = (e) => {
-      e.preventDefault();
-      const athleteName = e.target.getAttribute("name")
-      const athleteValue = e.target.value 
-
-      const newAthleteData = {...editAthleteData }
-      newAthleteData[athleteName] = athleteValue 
-
-      setEditAthleteData(newAthleteData)
-    }
+    setEditAthleteData(newAthleteData)
+  }
     
     
     
-      return (
-        <>
-          <h1> Athlete List {location.state.id} </h1>
-          <NewAthlete handleAddAthlete={handleAddAthlete} trailId={location.state.id}/>
-          {athletes.length > 0 && (
-            <form>
-              <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell></TableCell>
-                      <TableCell align="left">Name</TableCell>
-                      <TableCell align="right">Time</TableCell>
-                      <TableCell align="right">Unsupported</TableCell>
-                      <TableCell align="right">Actions</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {athletes.map((athlete) => (
-                      <>
-                      { editAthleteId === athlete.id ? (
-                        <EditAthleteList 
-                          editAthleteData={editAthleteData} 
-                          handleEditFormChange={handleEditFormChange}
-                        /> 
-                      ) : ( 
-                        <ReadAthleteList 
-                          athlete={athlete} 
-                          handleEditAthleteClick = {handleEditAthleteClick} 
-                          handleDeleteAthlete={handleDeleteAthlete} 
-                        />
-                      )}
-                        
-                        
-                      </>
-                    ))}
-                  </TableBody>
-                </Table>
-              </TableContainer> 
-          </form>
-          )}
-        </>
-      )
+  return (
+    <>
+      <h1> Athlete List {location.state.id} </h1>
+      <NewAthlete handleAddAthlete={handleAddAthlete} trailId={location.state.id}/>
+      {athletes.length > 0 && (
+        <form>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+              <TableHead>
+                <TableRow>
+                  <TableCell></TableCell>
+                  <TableCell align="left">Name</TableCell>
+                  <TableCell align="right">Time</TableCell>
+                  <TableCell align="right">Unsupported</TableCell>
+                  <TableCell align="right">Actions</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {athletes.map((athlete) => (
+                  <>
+                  { editAthleteId === athlete.id ? (
+                    <EditAthleteList 
+                      editAthleteData={editAthleteData} 
+                      handleEditFormChange={handleEditFormChange}
+                    /> 
+                  ) : ( 
+                    <ReadAthleteList 
+                      athlete={athlete} 
+                      handleEditAthleteClick = {handleEditAthleteClick} 
+                      handleDeleteAthlete={handleDeleteAthlete} 
+                    />
+                  )}
+                    
+                    
+                  </>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer> 
+      </form>
+      )}
+    </>
+  )
     
     
 }
